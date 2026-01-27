d845195fa280df7b95adfacaf5305fbc78d329aa
Mazen Naji <mazen.naji.1@gmail.com>
2026-01-27 12:18:19 +0200

    Add router integration and create Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/projects">Projects</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  padding: 20px;
+}
+
+a {
+  margin-right: 15px;
+}
+</style>
